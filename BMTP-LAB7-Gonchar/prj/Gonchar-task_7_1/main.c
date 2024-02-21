f8229c7cfa4b6b60834e289a166d0fc121ae253c
ArtemiyGonchar <artmiygonchar@gmail.com>
2024-02-21 19:39:03 +0200

    reworked lab

--- BMTP-LAB7-Gonchar/prj/Gonchar-task_7_1/main.c
+++ BMTP-LAB7-Gonchar/prj/Gonchar-task_7_1/main.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-#include <string.h>
-#include <ctype.h>
+#include <wchar.h>
+#include <wctype.h>
 
 int main() {
-    setlocale(LC_ALL, "");
+    setlocale(LC_ALL, "en_US.UTF-8");
 
-    char word[100];
+    wchar_t word[100];
     int count = 0;
-    char golosni[] = "ауеоюяіи";
-    printf("Введіть слово яке закінчується на %% , ~ або *\n");
-    scanf("%s", word);
-    char *substr = word;
+    wchar_t golosni[] = L"ауеоюяіиє";
+    wprintf(L"Введіть слово яке закінчується на %% , ~ або *\n");
+    wscanf(L"%ls", word);
+    wchar_t *substr = word;
 
-    while ((substr = strstr(substr, "ма")) != NULL) {
+    while ((substr = wcsstr(substr, L"ма")) != NULL) {
         count++;
         substr++;
     }
 
     if (count == 0) {
-        for (int i = 0; word[i] != '\0'; i++) {
-            char lower = tolower(word[i]);
-            for (int j = 0; golosni[j] != '\0'; j++) {
+        for (int i = 0; word[i] != L'\0'; i++) {
+            wchar_t lower = towlower(word[i]);
+            for (int j = 0; golosni[j] != L'\0'; j++) {
                 if (lower == golosni[j]) {
-                    printf("%c", word[i]);
+                    wprintf(L"%lc", word[i]);
                 }
             }
         }
-        printf("\n");
+        wprintf(L"\n");
     }
 
-    printf("Кількість ма: %d\n", count);
+    wprintf(L"Кількість ма: %d\n", count);
     return 0;
 }
